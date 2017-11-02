5fd5429bb69eb3e3983b2ee3292869a841413cdf
alanmacgowan <alanmacgowan@gmail.com>
2017-11-02 10:41:37 -0300

    Add project files.

---
+++ WebAppSecurity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppSecurity.Startup))]
+namespace WebAppSecurity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
